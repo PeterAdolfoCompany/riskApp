27400b81a75b4765105c0f5a916e752ddb21bbb4
Adolfo Vela <vela@tekplanner.com>
2018-10-24 16:28:09 -0500

    Listo sidebar que se oculta

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,35 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+// JS PARA EL NAV BAR -----------------------------
+
+$(".sidebar-dropdown > a").click(function() {
+  $(".sidebar-submenu").slideUp(200);
+  if (
+    $(this)
+      .parent()
+      .hasClass("active")
+  ) {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .parent()
+      .removeClass("active");
+  } else {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .next(".sidebar-submenu")
+      .slideDown(200);
+    $(this)
+      .parent()
+      .addClass("active");
+  }
+});
+
+$("#close-sidebar").click(function() {
+  $(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+  $(".page-wrapper").addClass("toggled");
+});
+// --------------------------------------------------
